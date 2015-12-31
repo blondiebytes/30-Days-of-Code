69f1b70ec3e1e3c7f686823e4a41890dd625a329
Kathryn Hodge <kahodge@vassar.edu>
2015-12-30 21:54:43 -0500

    Day 0 Code

---
+++ Video Series Code/Day 0/HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2015 blondiebytes
+ */
+package helloworld;
+
+/**
+ *
+ * @author kathrynhodge
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
