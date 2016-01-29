ee49218bd4d1f338fb9b6acc616037bfad484d1a
Mukul Mantosh <mukulmantosh@yahoo.com>
2016-01-29 14:47:49 +0530

    Added Day0 Day1 Day3 Solutions in cpp

---
+++ HackerRank Challenge Solutions/Day 0/HelloWorld.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cout<<"Hello World."<<endl;
+    cout<<"Welcome to 30 Days of Code.";
+    return 0;
+}
