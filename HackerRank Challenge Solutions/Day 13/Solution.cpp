e542597da1349ea6762d4f23fc3456a04d914fa8
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-01-19 10:58:05 -0400

    Added Day13 Solution in cpp

---
+++ HackerRank Challenge Solutions/Day 13/Solution.cpp
@@ -0,0 +1,50 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
+using namespace std;
+class Book{
+    protected:
+        string title;
+        string author;
+    public:
+        Book(string t,string a){
+            title=t;
+            author=a;
+        }
+        virtual void display()=0;
+
+};
+
+class MyBook: public Book
+{
+private:
+    int price;
+public:
+    MyBook(string title, string author, int price): Book(title, author)
+    {
+        this->price = price;
+    }
+
+    virtual void display()
+    {
+        cout << "Title: " << title << endl;
+        cout << "Author: " << author << endl;
+        cout << "Price: " << price << endl;
+    }
+};
+
+
+int main() {
+    string title,author;
+    int price;
+    getline(cin,title);
+    getline(cin,author);
+    cin>>price;
+    MyBook novel(title,author,price);
+    novel.display();
+    return 0;
+}
+
